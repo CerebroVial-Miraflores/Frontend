9c094376697ea39bdc4d537a72d170a21275a191
AndresBR2003 <bricenoandres1@hotmail.com>
2025-10-31 11:49:14 -0500

    CVGM-8 Creacion de estructura de carpetas

---
+++ src/pages/IntersectionDetails/IntersectionDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './IntersectionDetails.module.css';
+
+const IntersectionDetails = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Detalles de la Intersección</h1>
+    </div>
+  );
+};
+
+export default IntersectionDetails;
